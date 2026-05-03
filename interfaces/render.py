b956e6ddb6bceb2b656b402057a540a56a31a6fe
Joel Sheno <joe7885g@gmail.com>
2026-05-03 15:54:30 +0530

    Interaction changes for responsive feedback

--- interfaces/render.py
+++ interfaces/render.py
@@ -98,7 +98,8 @@ class RenderInterface(BaseScreen):
         self.update_states()
 
     def update_states(self):
-        has_file = self.current_file is not None
+        has_file = self.current_file is not None and os.path.exists(self.current_file)
+
         has_mode = bool(self.mode.get())
         is_loading = self.is_loading
 
